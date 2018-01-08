d4b1f33b6f11747af42bb1c4d60b993997ee5ddf
George Yammine <georgeyammine1994@gmail.com>
2018-01-07 21:57:15 -0500

    deployment issue fix

--- config/prodConfig.js
+++ config/prodConfig.js
@@ -1,12 +1,12 @@
 module.exports = {
 
     //Mongo
-    mongoURI: proccess.env.MONGO_URI,
+    mongoURI: process.env.MONGO_URI,
 
     //Cookie
-    cookieKey: proccess.env.COOKIE_KEY,
+    cookieKey: process.env.COOKIE_KEY,
     
     //Google
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    goolgleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET
+    goolgleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
