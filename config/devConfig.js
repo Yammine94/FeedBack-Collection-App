398555e409c33617872a463728b8ca6dc23943d3
George Yammine <georgeyammine1994@gmail.com>
2018-01-07 21:48:48 -0500

    err

---
+++ config/devConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    //Mongo
+    mongoURI: "mongodb://gyammine:Muaythai1994@ds245357.mlab.com:45357/feedbackapp",
+
+    //Cookie
+    cookieKey: "wfjawjfjawfjawjf0jaw0fja0jwfajwfiajwpjfpaopowjf",
+    
+    //Google
+    googleClientID: "13378230070-qocsa0v43kphrece94q1kcmr2kadl49e.apps.googleusercontent.com",
+    goolgleClientSecret: "HjxRX6MPIs-zDYi5KPFJwSbT"
+};
