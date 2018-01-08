93b67f0760449e5143f06602e872957acfe73dfa
George Yammine <georgeyammine1994@gmail.com>
2018-01-07 22:08:48 -0500

    server proxy

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.deserializeUser((tokenId, done) => {
 passport.use(new googleStrategy({
     clientID: config.googleClientID,
     clientSecret: config.goolgleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
     
     User.findOne({userId: profile.id})
