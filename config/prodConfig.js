9fc4da359c84c74f2c5c80ec25bfa94dc408a136
George Yammine <georgeyammine1994@gmail.com>
2018-01-07 21:34:15 -0500

    new prod only keys, fix deployment

---
+++ config/prodConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    //Mongo
+    mongoURI: proccess.env.MONGO_URI,
+
+    //Cookie
+    cookieKey: proccess.env.COOKIE_KEY,
+    
+    //Google
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    goolgleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET
+};
